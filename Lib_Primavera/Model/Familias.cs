dad46d3b6b1810afe8fa72af567a11ae8318e5bc
Rektosauros <mcmigas505@gmail.com>
2017-11-01 15:04:46 +0000

    Initial project version

---
+++ Lib_Primavera/Model/Familias.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class Familias
+    {
+        public string Familia { get; set; }
+        public string Descricao { get; set; }
+    }
+}
