12efd4921d373d7b15828d5642d92b5ff0e18a66
Rektosauros <mcmigas505@gmail.com>
2017-11-06 17:15:54 +0000

    routes changed

---
+++ Controllers/HomePageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstREST.Controllers
+{
+    public class HomePageController : Controller
+    {
+        //
+        // GET: /HomePage/
+
+        public String Index()
+        {
+            return "This is my <b>default<b/> action";
+        }
+
+        //
+        // GET: /HomePage/Welcome
+
+        public String Welcome()
+        {
+            return "This is my welcome action method";
+        }
+
+    }
+}
